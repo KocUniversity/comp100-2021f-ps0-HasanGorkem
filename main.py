f3cd971a1f00a5d76d65254995eaf1c1da7f6ba8
HasanGorkem <91727556+HasanGorkem@users.noreply.github.com>
2021-10-01 10:16:46 +0000

    oldu gibi

---
+++ main.py
@@ -0,0 +1,7 @@
+import math
+print("hello user \nplease enter numbers for function below \n x**y=?  and log(x)=?")
+x=int(input("please enter number x="))
+y=int(input("please enter number y="))
+print("x**y=",x**y)
+print("log(x)=",math.log2(x))
+print("0079798")
